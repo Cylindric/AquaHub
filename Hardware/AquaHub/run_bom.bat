161385d951cc07c560309a8b162bcc2efa9b19bc
Cylindric <mark@hanfordonline.co.uk>
2018-09-10 10:41:31 +0100

    Added latest output

--- Hardware/AquaHub/run_bom.bat
+++ Hardware/AquaHub/run_bom.bat
@@ -1,2 +1,4 @@
 @ECHO OFF
-"C:\Program Files\KiCad\bin\python.exe" C:\Users\passp\Downloads\kicad\InteractiveHtmlBom\InteractiveHtmlBom\generate_interactive_bom.py .\AquaHub.kicad_pcb
+"C:\Program Files\KiCad\bin\python.exe" C:\Users\passp\Downloads\kicad\InteractiveHtmlBom\InteractiveHtmlBom\generate_interactive_bom.py --nobrowser .\AquaHub.kicad_pcb
+MOVE bom\ibom.html ..\Output\
+ERASE /Q /S bom
